0b390a523c3a3ceff45195b22bb5586e82ab1bc1
sonu kumar mahto <sonumahtoraj121@gmail.com>
2024-04-23 19:16:12 +0530

    23.04.2024 Minimum Height Tree

--- LeetCode_Daily_Questions/April/src/com/April/Min_Height_Tree.java
+++ LeetCode_Daily_Questions/April/src/com/April/Min_Height_Tree.java
@@ -4,19 +4,19 @@ public class Min_Height_Tree {
     class Solution {
         public List<Integer> findMinHeightTrees(int n, int[][] edges) {
             //will use Modified BFS approach for Khans algorithm
-            List<Integer>ans = new ArrayList<>();
-            if(n<2){
-                for(int i=0;i<n;i++){
+            List<Integer> ans = new ArrayList<>();
+            if (n < 2) {
+                for (int i = 0; i < n; i++) {
                     ans.add(i);
                 }
                 return ans;
             }
-            ArrayList<Integer>[]adj = new ArrayList[n];
-            for(int i=0;i<n;i++){
+            ArrayList<Integer>[] adj = new ArrayList[n];
+            for (int i = 0; i < n; i++) {
                 adj[i] = new ArrayList<>();
             }
-            int[]degree = new int[n];
-            for(int[] e :edges){
+            int[] degree = new int[n];
+            for (int[] e : edges) {
                 int u = e[0];
                 degree[u]++;
                 int v = e[1];
@@ -24,22 +24,22 @@ public class Min_Height_Tree {
                 adj[u].add(v);
                 adj[v].add(u);
             }
-            Deque<Integer>que = new ArrayDeque<>();
-            for(int i=0;i<n;i++){
-                if(degree[i]==1){
+            Deque<Integer> que = new ArrayDeque<>();
+            for (int i = 0; i < n; i++) {
+                if (degree[i] == 1) {
                     que.add(i);
                 }
             }
-            while(!que.isEmpty()){
+            while (!que.isEmpty()) {
                 int sz = que.size();
                 ans = new ArrayList<>();
-                for(int i=0;i<sz;i++){
+                for (int i = 0; i < sz; i++) {
                     int u = que.poll();
                     ans.add(u);
                     degree[u]--;
-                    for(int v:adj[u]){
+                    for (int v : adj[u]) {
                         degree[v]--;
-                        if(degree[v]==1){
+                        if (degree[v] == 1) {
                             que.add(v);
                         }
                     }
@@ -47,4 +47,5 @@ public class Min_Height_Tree {
             }
             return ans;
         }
+    }
 }
