4b5d1511905a7b5b40d448c3acba233adbbab86a
sonu kumar mahto <sonumahtoraj121@gmail.com>
2024-05-09 23:46:45 +0530

    09.05.2025

---
+++ LeetCode_Daily_Questions/April/src/May_Practise/Maximum_Happiness.java
@@ -0,0 +1,16 @@
+package May_Practise;
+import java.util.*;
+public class Maximum_Happiness {
+    public long maximumHappinessSum(int[] arr, int k) {
+        Arrays.sort(arr);
+        long sum=0;
+        int j=0;
+        for(int i=arr.length-1;i>=0 && k>0;i--){
+            if(arr[i]-j>0){
+                sum+=arr[i]-j;
+            }
+            j++;k--;
+        }
+        return sum;
+    }
+}
